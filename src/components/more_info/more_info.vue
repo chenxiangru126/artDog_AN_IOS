9f1f51c41094f9271687c9a1eacf492013c255e2
zhangrujie <1468196979@qq.com>
2018-10-17 10:26:36 +0800

    more_info,10_17

--- src/components/more_info/more_info.vue
+++ src/components/more_info/more_info.vue
@@ -1,42 +1,51 @@
 <template>
     <div class="more_info">
+        <!-- 头部分享按钮 -->
+        <div class="header_if" >
+        <div class="_save" @click="toSaveInfo">保存</div>
+
+    </div>
         <div class="more_info_content">
             <div class="touxiang_box m1">
                 <div class="word word_1"  >头像</div>
                 <div class="pic pic_1">
-                    <img :src="avatar" style="width: 3.7rem;height: 3.7rem;border-radius:50%"  @click="trigger_file"  >
+                    <img v-if='!avatar'  @click="trigger_file" src="../../static/images/gerenzhongxintouxiang@3x.png" alt="">
+                    <img v-else :src="'http://test.bjyishubiyeji.com:8080'+avatar" style="width: 3.7rem;height: 3.7rem;border-radius:50%"  @click="trigger_file"  >
                 </div>
             </div>
             <div class="nicheng m1">
                 <div class="word ">昵称</div>
                 <div class="pic">
-                    <input type="text" placeholder="请输入..."> 
+                    <input type="text" placeholder="请输入..." style='color:#fff' v-model="nickName"> 
                 </div>
             </div>
             <div class="xingbie m1">
                 <div class="word">性别</div>
                 <div class="pic">
-                    <span>男</span>
+                    <span style='color:#fff'>男</span>
                     <span>></span>
                 </div>
             </div>
             <div class="shoujihao m1">
                 <div class="word">手机号</div>
                 <div class="pic">
-                    <span>未绑定</span>
+                    <span v-if='!telephone' >未绑定</span>
+                    <span v-else style='color:#fff'>{{telephone}}</span>
                     <span>></span>
                 </div>
             </div>
             <div class="qianming m1">
                 <div class="word">个性签名</div>
                 <div class="pic">
-                    <input type="text" placeholder="请输入..."> 
+                    <input type="text" placeholder="请输入..."  v-model="signature">
+                    <!-- <input v-if='!signature' type="text" placeholder="请输入..."> 
+                    <input v-else type="text" v-model="signature">  -->
                 </div>
             </div>
             <div class="leibie m1">
                 <div class="word">类别</div>
                 <div class="pic">
-                    <span>学生</span>
+                    <span @click='chooseType' style='color:#fff'>学生</span>
                     <span>></span>
                 </div>
             </div>
@@ -65,6 +74,8 @@
                 </div>
             </div>
         </div>
+        <!-- 类型选择框 -->
+        <div class=""></div>
     <!-- <input type="file" class="hide file" accept="image/*" @change="upload_img"> -->
     </div>
 </template>
@@ -72,7 +83,25 @@
 <style  lang="less">
 
 @import 'more_info.less';
+.header_if{
+    background: #232323;
+    width:100%;
+    position: fixed;
+    z-index: 100;
+    text-align: center;
+    top:1.25rem;
 
+    ._save{
+        width: 4rem;
+        height: 2.933333rem;
+        position: absolute;
+        right: 0;
+        top: 0;
+        color: #fff;
+        line-height: 2.933333rem;
+    }
+
+}
 </style>
 
 
@@ -86,11 +115,26 @@ export default {
 
     data(){
         return{
-        avatar:null,
+            avatar:null,
+            nickName:'',
+            telephone:'',
+            signature:'',
         }
 
     },
+    created(){
+        // 初始化信息加载
+        this.util.ajax.get('/admin/sysUser/getUserById.do?alert=0').then(e=> {
+            this.nickName = e.bean.nickName
+            this.telephone = e.bean.phone
+            this.signature = e.bean.signature
+            this.avatar = e.bean.photo	
+            
+         }) 
+        
+    },
     methods:{
+        // 上传头像
         trigger_file() {
             // debugger
             znt.selectImage({
@@ -104,7 +148,15 @@ export default {
                     alert(res.msg)
                 }
             });
-}
+        },
+        // 选择类别
+        chooseType(){
+
+        },
+        // 保存信息
+        toSaveInfo(){
+
+        }
             
 
 
